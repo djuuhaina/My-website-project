4df114433012a4940fdea7820e52e8dbce2d3e91
DJIHANE AMRANE <168914054+djuuhaina@users.noreply.github.com>
2024-05-06 09:31:12 +0100

    Add files via upload
    Students : AMRANE DJIHANE
                    BEGHOURA RAHIMA
    Group : 02

---
+++ project daw/script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => { 
+    header.classList.toggle("show-mobile-menu");
+    header.click();
+});
